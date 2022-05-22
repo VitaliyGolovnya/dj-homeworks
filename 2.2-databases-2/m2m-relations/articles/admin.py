ccc6ae230c6c28b5776c67a69fb9bcb9f10c37df
Golovnya Vitaliy <vitas210295@gmail.com>
2022-05-22 16:08:20 +1000

    databases-2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main'] == True:
+                count +=1
+        if count != 1:
+            raise ValidationError('Выберите один основной раздел')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
